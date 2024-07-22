27f876c9187702cc6136c97d50442c34c972c238
Hirusha Dheemantha <123639147+HirushaDheemantha@users.noreply.github.com>
2024-07-22 07:04:41 +0530

    Initial commit

---
+++ user.php
@@ -0,0 +1,61 @@
+<?php 
+  
+  include("connect.php");
+
+?>
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+   <div class="container my-5">
+   <form method="post">
+        <div class="form-group mb-3">
+          <label>Name</label>
+          <input type="text" name="name" class="form-control" placeholder="Enter your name">
+        </div>   
+        <div class="form-group mb-3 ">
+          <label>Email</label>
+          <input type="text" name="email" class="form-control" placeholder="Enter your email">
+        </div>   
+        <div class="form-group mb-3">
+          <label>Mobile</label>
+          <input type="text" name="mobile" class="form-control" placeholder="Mobile number">
+        </div>   
+        <div class="form-group mb-3">
+          <label>Password</label>
+          <input type="password" name="password" class="form-control" placeholder="password">
+        </div>   
+  <button name="btnsubmit" type="submit" class="btn btn-primary">Submit</button>
+</form>
+   </div>
+  </body>
+</html>
+
+<?php 
+  if(isset($_POST['btnsubmit'])){
+    $name= $_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $password=$_POST['password'];
+
+    $sql = "insert into `crud` (name,email,mobile,password) values('$name','$email','$mobile','$password')";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+      echo "data inserted successful";
+    }
+    
+  }
+
+
+?>
